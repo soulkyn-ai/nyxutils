a2ebfe7ac3036f47ec16808e7c32d53f7d72e45d
Gasquez Florian <m@fy.to>
2024-10-19 20:36:58 +0200

    wakeup

--- cronjob.go
+++ cronjob.go
@@ -50,19 +50,22 @@ func (h *JobHeap) Pop() interface{} {
 }
 
 type CronManager struct {
-	jobHeap  JobHeap
-	lock     sync.Mutex
-	stopChan chan struct{}
-	running  bool
-	logger   *zerolog.Logger
-	wg       sync.WaitGroup // Added WaitGroup to track dispatcher
+	jobHeap    JobHeap
+	lock       sync.Mutex
+	wakeUpChan chan struct{}
+	stopChan   chan struct{}
+	running    bool
+	logger     *zerolog.Logger
+	wg         sync.WaitGroup // For dispatcher
+	jobWg      sync.WaitGroup // For jobs
 }
 
 func NewCronManager(logger *zerolog.Logger) *CronManager {
 	return &CronManager{
-		jobHeap:  make(JobHeap, 0),
-		stopChan: make(chan struct{}),
-		logger:   logger,
+		jobHeap:    make(JobHeap, 0),
+		wakeUpChan: make(chan struct{}, 1), // Buffered channel
+		stopChan:   make(chan struct{}),
+		logger:     logger,
 	}
 }
 
@@ -75,8 +78,6 @@ func (m *CronManager) AddCron(name string, job Job, interval time.Duration, runI
 	}
 
 	m.lock.Lock()
-	defer m.lock.Unlock()
-
 	if runImmed {
 		cronJob.nextRun = time.Now()
 	} else {
@@ -84,17 +85,21 @@ func (m *CronManager) AddCron(name string, job Job, interval time.Duration, runI
 	}
 
 	heap.Push(&m.jobHeap, cronJob)
+	m.lock.Unlock()
 	m.logger.Debug().Msgf("[cron|%s|%s] Added as job", name, interval)
 
 	// Signal to the dispatcher that a new job is available.
 	if m.running {
-		// Close and recreate stopChan to interrupt the dispatcher timer.
-		close(m.stopChan)
-		m.stopChan = make(chan struct{})
+		select {
+		case m.wakeUpChan <- struct{}{}:
+		default:
+		}
 	}
 }
 
 func (m *CronManager) runJob(cronJob *CronJob) {
+	m.jobWg.Add(1)
+	defer m.jobWg.Done()
 	defer func() {
 		if r := recover(); r != nil {
 			m.logger.Error().Msgf("[cron|%s|%s] panicked: %v", cronJob.Name, cronJob.Interval, r)
@@ -120,42 +125,48 @@ func (m *CronManager) dispatcher() {
 		m.lock.Lock()
 		if len(m.jobHeap) == 0 {
 			m.lock.Unlock()
-			// Wait for a new job to be added or stop signal.
 			select {
 			case <-m.stopChan:
 				return
+			case <-m.wakeUpChan:
+				// New job added, continue
 			}
-		} else {
-			// Get the job with the earliest nextRun time.
-			nextJob := m.jobHeap[0]
-			now := time.Now()
-			delay := nextJob.nextRun.Sub(now)
+			continue
+		}
+
+		nextJob := m.jobHeap[0]
+		now := time.Now()
+		delay := nextJob.nextRun.Sub(now)
+		m.lock.Unlock()
+
+		if delay <= 0 {
+			m.lock.Lock()
+			if !m.running {
+				m.lock.Unlock()
+				return
+			}
+			heap.Pop(&m.jobHeap)
 			m.lock.Unlock()
 
-			if delay <= 0 {
-				// Time to run the job.
-				m.lock.Lock()
-				heap.Pop(&m.jobHeap)
-				m.lock.Unlock()
+			m.runJob(nextJob)
 
-				m.runJob(nextJob)
+			// Reschedule the job.
+			nextJob.nextRun = time.Now().Add(nextJob.Interval)
 
-				// Reschedule the job.
-				nextJob.nextRun = time.Now().Add(nextJob.Interval)
-
-				m.lock.Lock()
-				heap.Push(&m.jobHeap, nextJob)
-				m.lock.Unlock()
-			} else {
-				// Wait until the next job's scheduled time.
-				timer := time.NewTimer(delay)
-				select {
-				case <-timer.C:
-					// Time to check the jobs again.
-				case <-m.stopChan:
-					timer.Stop()
-					return
-				}
+			m.lock.Lock()
+			heap.Push(&m.jobHeap, nextJob)
+			m.lock.Unlock()
+		} else {
+			timer := time.NewTimer(delay)
+			select {
+			case <-timer.C:
+				// Time to check the jobs again.
+			case <-m.stopChan:
+				timer.Stop()
+				return
+			case <-m.wakeUpChan:
+				timer.Stop()
+				// New job added, continue
 			}
 		}
 	}
@@ -168,8 +179,9 @@ func (m *CronManager) Start() {
 		return
 	}
 	m.running = true
+	m.wakeUpChan = make(chan struct{}, 1) // Buffered channel
 	m.stopChan = make(chan struct{})
-	m.wg.Add(1) // Added to track dispatcher
+	m.wg.Add(1) // For dispatcher
 	m.lock.Unlock()
 	go func() {
 		m.dispatcher()
@@ -186,5 +198,6 @@ func (m *CronManager) Stop() {
 	m.running = false
 	close(m.stopChan)
 	m.lock.Unlock()
-	m.wg.Wait() // Wait for dispatcher to finish
+	m.wg.Wait()    // Wait for dispatcher to finish
+	m.jobWg.Wait() // Wait for running jobs to finish
 }
